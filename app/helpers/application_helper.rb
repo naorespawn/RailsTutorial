dd2ebabed4176e3d2b0eaf1040190b84d9fe3ba5
Naoki Egawa <nao.respawn@gmail.com>
2019-10-05 10:07:12 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,10 +9,23 @@ module ApplicationHelper
   end
   
   def palindrome_tester(s)
-    if s == s.reverse
+    if self == reverse
       puts "It's a palindrome!"
     else
       puts "It's not a palindrome."
     end
   end
+  
+  def yeller(a)
+    a.join.upcase
+  end
+  
+  def randam_subdomain
+    ('a'..'z').to_a.shuffle[0..7].join
+  end
+  
+  def string_shuffle(s)
+     s.split('').shuffle.join
+  end
+  
 end

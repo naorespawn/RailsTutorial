362d0b8475b764cb1f5fec209d3f3c4710114015
Naoki Egawa <nao.respawn@gmail.com>
2019-10-23 11:11:40 +0000

    Remember_me test

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,21 +9,20 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   
-  class << self
-    # 渡された文字列のハッシュ値を返す
-    def digest(string)
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                            BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
-    end
-    
-    # ランダムなトークンを返す
-    def new_token
-      SecureRandom.urlsafe_base64
-    end
+  # 渡された文字列のハッシュ値を返す
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def self.new_token
+    SecureRandom.urlsafe_base64
   end
   
-    # 永続セッションのためにユーザーをデータベースに記憶する
+  
+  # 永続セッションのためにユーザーをデータベースに記憶する
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))

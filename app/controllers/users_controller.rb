5780d40a990c106b62e954958e721d201b74b703
Naoki Egawa <nao.respawn@gmail.com>
2019-11-04 11:58:09 +0000

    ユーザー削除のテストを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,7 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless @user == current_user
     end
     
+    # 管理者かどうか確認
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end

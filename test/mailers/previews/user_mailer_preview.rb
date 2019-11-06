5c579ba5b0e73f024a5222b5c20404f2c26eba3b
Naoki Egawa <nao.respawn@gmail.com>
2019-11-06 10:55:36 +0000

    account_activations_controllerのeditアクションを作成

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,12 +1,14 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+# Preview all emails at https://757f05b9154e41bcb15965786f4264ee.vfs.cloud9.us-east-2.amazonaws.com/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+  # Preview this email at https://757f05b9154e41bcb15965786f4264ee.vfs.cloud9.us-east-2.amazonaws.com/rails/mailers/user_mailer/account_activation
   def account_activation
-    UserMailer.account_activation
+    user = User.first
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
   end
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  # Preview this email at https://757f05b9154e41bcb15965786f4264ee.vfs.cloud9.us-east-2.amazonaws.com/rails/mailers/user_mailer/password_reset
   def password_reset
     UserMailer.password_reset
   end

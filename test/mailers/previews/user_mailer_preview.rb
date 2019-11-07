239d0c060482986864f970c87b66bbd6c31829a4
Naoki Egawa <nao.respawn@gmail.com>
2019-11-07 13:53:58 +0000

    パスワードリセットのメールを送信できるように

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -10,7 +10,9 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at https://757f05b9154e41bcb15965786f4264ee.vfs.cloud9.us-east-2.amazonaws.com/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 
 end

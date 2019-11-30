85c1860c228bdfdbbdf094e5d333079555f5f88b
Naoki Egawa <nao.respawn@gmail.com>
2019-11-30 07:12:18 +0000

    UserMicropostを追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,7 +9,7 @@ class Micropost < ApplicationRecord
   private
     #アップロードされた画像のサイズをバリデーションする
     def picture_size
-      if picture_size > 5.megabytes
+      if picture.size > 5.megabytes
         errors.add(:picture, "should be less than 5MB")
       end
     end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -13,6 +13,8 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+  # アップロードファイルの保存先ディレクトリは上書き可能
+  # 下記はデフォルトの保存先  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
